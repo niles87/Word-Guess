c4909d86c18ee56370c373bbdff18392fb0c9e91
niles87 <nbing19@gmail.com>
2019-12-10 18:51:21 -0800

    added if statements to change img and play audio

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -45,7 +45,7 @@ function resetGame() {
     currentWord = [];
 
     // Get a new word
-    guessCount = Math.floor(wordPossibilites.length * 1.5);
+    guessCount = Math.floor(wordPossibilites.length);
     console.log(guessCount);
     updateDisplay();
 
@@ -88,27 +88,63 @@ function checkForLetter(letter) {
         console.log("Thats not correct try again!")
         if (!guesses.includes(letter)) {
             guesses.push(letter);
+            guessCount -= 1;
             updateDisplay()
         }
     }
-    guessCount -= 1;
+    
     winOrLose();
 }
 
 function winOrLose() {
     if (currentWord.join("") === wordPossibilites && guessCount > 0) {
+        var img = document.getElementById("image");
+        var audio = document.getElementById("audio");
         userWins++;
         console.log("---below is the current wins on the page---");
         console.log(userWins);
         pauseGame = true;
         console.log("You won!");
         updateDisplay();
+        
+        if (wordPossibilites === choices[0]) {
+            img.src = "assets/images/cranberries.jpeg"
+            audio.src = "assets/images/The-Cranberries-Zombie.mp3"
+        } else if (wordPossibilites === choices[1]) {
+            img.src = "assets/images/soundgarden.jpeg"
+            audio.src = "assets/images/Soundgarden-Black-Hole-Sun-With.mp3"
+        } else if (wordPossibilites === choices[2]) {
+            img.src = "assets/images/radiohead.jpeg"
+            audio.src = "assets/images/Radiohead-Creep.mp3"
+        } else if (wordPossibilites === choices[3]) {
+            img.src = "assets/images/oasis.jpeg"
+            audio.src = "assets/images/Oasis-Wonderwall.mp3"
+        } else if (wordPossibilites === choices[4]) {
+            img.src = "assets/images/nirvana.jpeg"
+            audio.src = "assets/images/Nirvana-Smells-Like-Teen-Spirit.mp3"
+        } else if (wordPossibilites === choices[5]) {
+            img.src = "assets/images/weezer.jpeg"
+            audio.src = "assets/images/Weezer-BuddyHolly.mp3"
+        } else if (wordPossibilites === choices[6]) {
+            img.src = "assets/images/blur.jpeg"
+            audio.src = "assets/images/Blur-Song-2.mp3"
+        } else if (wordPossibilites === choices[7]) {
+            img.src = "assets/images/candlebox.jpeg"
+            audio.src = "assets/images/Candlebox-FarBehind.mp3"
+        } else if (wordPossibilites === choices[8]) {
+            img.src = "assets/images/foofighters.png"
+            audio.src = "assets/images/FooFighters-Everlong.mp3"
+        } else if (wordPossibilites === choices[9]) {
+            img.src = "assets/images/thirdeyeblind.jpeg"
+            audio.src = "assets/images/ThirdEyeBlind-Semi-Charmed-Life.mp3"
+        }
         resetGame();
     }
-    if (guessCount === 0 && currentWord !== wordPossibilites) {
+    if (guessCount === 0) {
         currentWord = wordPossibilites.split();
         pauseGame = true;
         console.log("you lost try again");
         resetGame();
     }
+    
 }
